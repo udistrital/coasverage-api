9f3024b4e9ced5e307fe2541672b4ca7956abdef
andresvia <andres.via@gmail.com>
2017-01-17 01:07:08 -0500

    Creating Some Basic Routes

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
@@ -11,10 +10,17 @@ import (
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	router.HandleFunc("/coverages", CoverageIndex)
+	router.HandleFunc("/coverage/{id}", Coverage)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q!", html.EscapeString(r.URL.Path))
+func CoverageIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Coverage Index!")
+}
+
+func Coverage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	coverage_id := vars["id"]
+	fmt.Fprintf(w, "Coverage id: %v\n", coverage_id)
 }
