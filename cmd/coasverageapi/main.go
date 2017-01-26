cd3abbe20bddae8ef194f2feb05ad6aa52c49c89
andresvia <andres.via@gmail.com>
2017-01-25 23:07:01 -0500

    handling cors with gorilla, preparing for 'gin run run'

--- cmd/coasverageapi/main.go
+++ cmd/coasverageapi/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"github.com/gorilla/handlers"
 	"github.com/udistrital/coasverage-api/internal/coasverageapi"
-	"log"
+	"github.com/urfave/cli"
 	"net/http"
 	"os"
 )
 
-func main() {
-	var listen string
-	var listen bool
-	listen, listen_found = os.LookupEnv("LISTEN")
-	if !listen_found {
-		listen = "127.0.0.1:8000"
+var app *cli.App
+var listen string
+var port string
+var cors_allowed_origins cli.StringSlice = []string{"*"}
+
+func init() {
+	app = cli.NewApp()
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name: "run",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:        "listen",
+					EnvVar:      "LISTEN",
+					Destination: &listen,
+					Value:       "127.0.0.1",
+				},
+				cli.StringFlag{
+					Name:        "port",
+					EnvVar:      "PORT",
+					Destination: &port,
+					Value:       "3000",
+				},
+				cli.StringSliceFlag{
+					Name:   "cors_allowed_origins",
+					EnvVar: "CORS_ALLOWED_ORIGINS",
+					Value:  &cors_allowed_origins,
+				},
+			},
+			Action: action,
+		},
 	}
-	log.Fatal(http.ListenAndServe(listen_and_serve, coasverageapi.NewRouter()))
+}
+
+func action(ctx *cli.Context) (err error) {
+	router := coasverageapi.NewRouter()
+	handler := handlers.CORS(handlers.AllowedOrigins(cors_allowed_origins))(router)
+	return http.ListenAndServe(listen+":"+port, handler)
+}
+
+func main() {
+	app.Run(os.Args)
 }
